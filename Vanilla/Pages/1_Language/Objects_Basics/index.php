d0ec0b5e4b99f6ca062afb3437641177edca4e96
pew-rivza <dz-10b@ya.ru>
2020-04-19 23:30:32 +0300

    created section "Objects_Basics"

---
+++ Vanilla/Pages/1_Language/Objects_Basics/index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Katrin
+ * Date: 19.04.2020
+ * Time: 23:08
+ */
+
+include_once __DIR__ . "/../../common/Head.php";
+?>
+
+<div class="chapter-container">
+    <div class="menu">
+        <h3>Навигация</h3>
+        <ul>
+            <li><a href="#modal-functions">Объекты</a></li>
+            <li><a href="#if-else">Сборка мусора</a></li>
+            <li><a href="#logical-operators">Тип данных Symbol</a></li>
+            <li><a href="#cycles">Методы объекта, "this"</a></li>
+            <li><a href="#switch">Преобразование объектов в примитивы</a></li>
+            <li><a href="#switch">Конструкторы, создание объектов через "new"</a></li>
+        </ul>
+    </div>
+    <div class="content">
+        <h2>Объекты: основы</h2>
+        <? include_once __DIR__ . "/themes/Objects.php"; ?>
+    </div>
+</div>
---
+++ Vanilla/Pages/1_Language/Objects_Basics/themes/Objects.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Katrin
+ * Date: 19.04.2020
+ * Time: 23:26
+ */
+?>
+
+<script src="/Vanilla/Code/1_Language/Objects_Basics/Objects.js?<?= time();?>"></script>
+
+<div class="theme">
+    <h3 id="logical-operators">Объекты</h3>
+    <div class="task">
+        <div class="header">Задача 1</div>
+        Написать код, который будет спрашивать логин с помощью prompt. Если
+        посетитель вводит «Админ», то prompt запрашивает пароль, если ничего
+        не введено или нажата клавиша Esc – показать «Отменено», в противном
+        случае отобразить «Я вас не знаю».<br>
+        Пароль проверять так:<br>
+        Если введён пароль «Я главный», то выводить «Здравствуйте!»,<br>
+        Иначе – «Неверный пароль»,<br>
+        При отмене – «Отменено».
+
+        <div class="header">Решение</div>
+        <button onclick="">Войти</button>
+    </div>
+</div>
