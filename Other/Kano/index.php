96e7a00f896ce1bb3820a7e8eec5febbb6709830
pew-rivza <dz-10b@ya.ru>
2020-04-23 23:29:33 +0300

    site for Kano created

---
+++ Other/Kano/index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Katrin
+ * Date: 23.04.2020
+ * Time: 23:17
+ */
+
+header("Content-Type: text/html; charset=utf-8");
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+?>
+
+<head>
+    <title>Анализ по модели Кано</title>
+    <script
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
+</head>
+<body>
+
+</body>
