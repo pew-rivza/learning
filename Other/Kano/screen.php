898c8257d8b7037d095a243ded9800b6441ab102
pew-rivza <dz-10b@ya.ru>
2020-04-25 16:40:54 +0300

    Kano main screen created

---
+++ Other/Kano/screen.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Katrin
+ * Date: 24.04.2020
+ * Time: 21:02
+ */
+?>
+
+<div class="screen-container page" id="screen">
+    <div class="overlay">
+        <h1>Пройдите опрос по функционалу приложения</h1>
+        <button class="link" data-href="#description">Поехали!</button>
+    </div>
+    <video loop="" muted="" autoplay="" poster="/Other/Kano/assets/img/background-poster.jpg" class="video">
+        <source src="/Other/Kano/assets/files/background.mp4" type="video/mp4">
+    </video>
+</div>
