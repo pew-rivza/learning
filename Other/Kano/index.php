8ce8e5c8263c952f328746367b7a6fcf465d4c25
pew-rivza <dz-10b@ya.ru>
2020-04-26 00:06:49 +0300

    Kano thanks-page created

--- Other/Kano/index.php
+++ Other/Kano/index.php
@@ -20,6 +20,7 @@ ini_set('display_startup_errors', 1);
     <link rel="stylesheet" href="/Other/Kano/assets/css/screen.css"/>
     <link rel="stylesheet" href="/Other/Kano/assets/css/description.css"/>
     <link rel="stylesheet" href="/Other/Kano/assets/css/question.css"/>
+    <link rel="stylesheet" href="/Other/Kano/assets/css/thanks.css"/>
 
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;900&display=swap"
           rel="stylesheet">
@@ -30,29 +31,30 @@ ini_set('display_startup_errors', 1);
     <script src="/Other/Kano/assets/js/main.js"></script>
 </head>
 
-<?php include_once __DIR__ . "/screen.php"; ?>
-<?php include_once __DIR__ . "/description.php"; ?>
-<?php include_once __DIR__ . "/questions/q1.php"; ?>
-<?php include_once __DIR__ . "/questions/q2.php"; ?>
-<?php include_once __DIR__ . "/questions/q3.php"; ?>
-<?php include_once __DIR__ . "/questions/q4.php"; ?>
-<?php include_once __DIR__ . "/questions/q5.php"; ?>
-<?php include_once __DIR__ . "/questions/q6.php"; ?>
-<?php include_once __DIR__ . "/questions/q7.php"; ?>
-<?php include_once __DIR__ . "/questions/q8.php"; ?>
-<?php include_once __DIR__ . "/questions/q9.php"; ?>
-<?php include_once __DIR__ . "/questions/q10.php"; ?>
-<?php include_once __DIR__ . "/questions/q11.php"; ?>
-<?php include_once __DIR__ . "/questions/q12.php"; ?>
-<?php include_once __DIR__ . "/questions/q13.php"; ?>
-<?php include_once __DIR__ . "/questions/q14.php"; ?>
-<?php include_once __DIR__ . "/questions/q15.php"; ?>
-<?php include_once __DIR__ . "/questions/q16.php"; ?>
-<?php include_once __DIR__ . "/questions/q17.php"; ?>
-<?php include_once __DIR__ . "/questions/q18.php"; ?>
-<?php include_once __DIR__ . "/questions/q19.php"; ?>
-<?php include_once __DIR__ . "/questions/q20.php"; ?>
-<?php include_once __DIR__ . "/questions/q21.php"; ?>
-<?php include_once __DIR__ . "/questions/q22.php"; ?>
-<?php include_once __DIR__ . "/questions/q23.php"; ?>
+<?php //include_once __DIR__ . "/screen.php"; ?>
+<?php //include_once __DIR__ . "/description.php"; ?>
+<?php //include_once __DIR__ . "/questions/q1.php"; ?>
+<?php //include_once __DIR__ . "/questions/q2.php"; ?>
+<?php //include_once __DIR__ . "/questions/q3.php"; ?>
+<?php //include_once __DIR__ . "/questions/q4.php"; ?>
+<?php //include_once __DIR__ . "/questions/q5.php"; ?>
+<?php //include_once __DIR__ . "/questions/q6.php"; ?>
+<?php //include_once __DIR__ . "/questions/q7.php"; ?>
+<?php //include_once __DIR__ . "/questions/q8.php"; ?>
+<?php //include_once __DIR__ . "/questions/q9.php"; ?>
+<?php //include_once __DIR__ . "/questions/q10.php"; ?>
+<?php //include_once __DIR__ . "/questions/q11.php"; ?>
+<?php //include_once __DIR__ . "/questions/q12.php"; ?>
+<?php //include_once __DIR__ . "/questions/q13.php"; ?>
+<?php //include_once __DIR__ . "/questions/q14.php"; ?>
+<?php //include_once __DIR__ . "/questions/q15.php"; ?>
+<?php //include_once __DIR__ . "/questions/q16.php"; ?>
+<?php //include_once __DIR__ . "/questions/q17.php"; ?>
+<?php //include_once __DIR__ . "/questions/q18.php"; ?>
+<?php //include_once __DIR__ . "/questions/q19.php"; ?>
+<?php //include_once __DIR__ . "/questions/q20.php"; ?>
+<?php //include_once __DIR__ . "/questions/q21.php"; ?>
+<?php //include_once __DIR__ . "/questions/q22.php"; ?>
+<?php //include_once __DIR__ . "/questions/q23.php"; ?>
+<?php include_once __DIR__ . "/thanks.php"; ?>
 
---
+++ Other/Kano/thanks.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Katrin
+ * Date: 25.04.2020
+ * Time: 23:41
+ */
+?>
+
+<div class="page thanks-container" id="thanks">
+    <div class="overlay">
+        <h1>Спасибо!</h1>
+        <h2>Данные опроса отправлены</h2>
+    </div>
+    <video loop="" muted="" autoplay="" poster="/Other/Kano/assets/img/background-poster.jpg" class="video">
+        <source src="/Other/Kano/assets/files/thanks.mp4" type="video/mp4">
+    </video>
+</div>
