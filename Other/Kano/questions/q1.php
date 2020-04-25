f47ff057b0151b1a9ffa0494431ba0c4d83bfb4d
pew-rivza <dz-10b@ya.ru>
2020-04-25 17:11:23 +0300

    Kano question template layout refactoring

--- Other/Kano/questions/q1.php
+++ Other/Kano/questions/q1.php
@@ -16,7 +16,10 @@
 
     <div class="content">
         <h2>Функция</h2>
-        <div class="function">Добавление одежды в гардероб не по одной, а по несколько штук за раз</div>
+        <div class="function">
+            Добавление одежды в гардероб не по одной, а по несколько штук за раз
+        </div>
+
         <div class="answers">
             <div class="option">Насколько вам понравится, если в продукте будет эта функция?</div>
             <div class="variant" data-code="1-1-1">Было бы круто иметь эту функцию!</div>
@@ -25,6 +28,7 @@
             <div class="variant" data-code="1-1-4">Ну, я не в восторге, но переживу</div>
             <div class="variant" data-code="1-1-5">Дичь какая-то, я не буду этим пользоваться!</div>
         </div>
+
         <div class="answers">
             <div class="option">Как вы отнесетесь к тому, если в продукте этой функции не будет вообще?</div>
             <div class="variant" data-code="1-2-1">Было бы круто, если бы этой функции не было!</div>
@@ -33,6 +37,7 @@
             <div class="variant" data-code="1-2-4">Ну, я не в восторге, но переживу</div>
             <div class="variant" data-code="1-2-5">Дичь какая-то, эта функция необходима!</div>
         </div>
+
         <div class="buttons-bar">
             <button class="link light" data-href="#description">Назад</button>
             <button class="link" data-href="">Далее</button>
