b48e11ef75da3109f10ed9850be2c69737847517
pew-rivza <dz-10b@ya.ru>
2020-05-01 17:30:41 +0300

    js numbers theme

--- Vanilla/Code/1_Language/Data_Types/Numbers.js
+++ Vanilla/Code/1_Language/Data_Types/Numbers.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function getRandom(randomFunction) {
+    let min = "";
+    while(!(min === "0" || +min || min === null)) {
+        min = prompt("Введите МИНИМАЛЬНОЕ значение диапазона");
+    }
+
+    let max = "";
+    if (min !== null) {
+        while(!(max === "0" || +max || max === null)) {
+            max = prompt("Введите МАКСИМАЛЬНОЕ значение диапазона");
+        }
+    }
+
+    if (min !== null && max !== null) {
+        let tryMore;
+
+        do {
+            tryMore = confirm(randomFunction(min, max));
+        }
+        while(tryMore);
+    }
+}
+
+function random(min, max) {
+    return Math.random() * (max - min) + +min;
+}
+
+function randomInt(min, max) {
+    return parseInt(
+        '' +
+        ( Math.random() * (max - min + 1) + +min )
+    );
+}
--- Vanilla/Pages/1_Language/Data_Types/themes/Numbers.php
+++ Vanilla/Pages/1_Language/Data_Types/themes/Numbers.php
@@ -13,13 +13,20 @@
     <h3 id="numbers">Числа</h3>
     <div class="task">
         <div class="header">Задача 1</div>
-        Используя условную конструкцию, написать код, который будет спрашивать:
-        „Какое «официальное» название JavaScript?“. Если пользователь вводит
-        «ECMAScript», то показать: «Верно!», в противном случае – отобразить:
-        «Не знаете? ECMAScript!».
+        Написать функцию random(min, max), которая генерирует случайное число
+        с плавающей точкой от min до max (но не включая max).
 
         <div class="header">Решение</div>
-        <button onclick="">Сыграть в JS-викторину</button>
+        <button onclick="getRandom(random);">Вывести случайное число</button>
+    </div>
+
+    <div class="task">
+        <div class="header">Задача 2</div>
+        Написать функцию randomInt(min, max), которая генерирует случайное целое
+        число от min до max (включительно).
+
+        <div class="header">Решение</div>
+        <button onclick="getRandom(randomInt);">Вывести случайное целое число</button>
     </div>
 </div>
 
