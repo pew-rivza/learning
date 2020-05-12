c019155cec34d9d88fc20a97c3d80addff37f227
pew-rivza <dz-10b@ya.ru>
2020-05-12 22:16:16 +0300

    JS RecursionNStack theme created

---
+++ Vanilla/Pages/1_Language/Functions_Advanced/index.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once __DIR__ . "/../../common/Head.php";
+include_once __DIR__ . "/../../common/Menu.php";
+?>
+
+<div class="chapter-container">
+    <div class="sub-menu">
+        <h3>Навигация</h3>
+        <ul>
+            <li><a href="#recursion-n-stack">Рекурсия и стек</a></li>
+            <li><a href="#">Остаточные параметры и оператор расширения</a></li>
+            <li><a href="#">Замыкание</a></li>
+            <li><a href="#">Устаревшее ключевое слово "var"</a></li>
+            <li><a href="#">Глобальный объект</a></li>
+            <li><a href="#">Объект функции, NFE</a></li>
+            <li><a href="#">Синтаксис "new Function"</a></li>
+            <li><a href="#">Планирование: setTimeout и setInterval</a></li>
+            <li><a href="#">Декораторы и переадресация вызова, сall/apply</a></li>
+            <li><a href="#">Привязка контекста к функции</a></li>
+            <li><a href="#">Повторяем стрелочные функции</a></li>
+        </ul>
+    </div>
+    <div class="content">
+        <h2>Продвинутая работа с функциями</h2>
+        <? include_once __DIR__ . "/themes/RecursionNStack.php"; ?>
+    </div>
+</div>
---
+++ Vanilla/Pages/1_Language/Functions_Advanced/themes/RecursionNStack.php
@@ -0,0 +1,11 @@
+<script src="/Vanilla/Code/1_Language/Functions_Advanced/RecursionNStack.js"></script>
+
+<div class="theme">
+    <h3 id="recursion-n-stack">Рекурсия и стек</h3>
+    <div class="task">
+        <div class="header">Задача 1</div>
+
+        <div class="header">Решение</div>
+        <button onclick="">DATA</button>
+    </div>
+</div>
