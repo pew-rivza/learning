e3a539a622a7e660f3804b48f30d21ebc5ad09fa
pew-rivza <dz-10b@ya.ru>
2020-05-14 00:04:19 +0300

    JS fibonacci function created

--- Vanilla/Code/1_Language/Functions_Advanced/RecursionNStack.js
+++ Vanilla/Code/1_Language/Functions_Advanced/RecursionNStack.js
@@ -7,13 +7,13 @@ function sumToNCycle(number) {
         sum += i;
     }
 
-    return {sum, method: "цикл"};
+    return {result: sum, method: "цикл"};
 }
 
 function sumToNRecursion(number) {
     let sum = sumTo(number);
 
-    return {sum, method: "рекурсия"};
+    return {result: sum, method: "рекурсия"};
 }
 
 function sumTo(n) {
@@ -23,10 +23,40 @@ function sumTo(n) {
 function sumToNProgression(number) {
     let sum = number * ((1 + number) / 2);
 
-    return {sum, method: "арифметическая прогрессия"};
+    return {result: sum, method: "арифметическая прогрессия"};
 }
 
+function fibCycle(number) {
+    let a = 1;
+    let b = 1;
 
+    if (number === 0) return {result: number, method: "цикл"};
+    if (number === 1 || number === 2) return {result: 1, method: "цикл"};
+
+    for (let i = 3; i <= number; i++) {
+        let c = a + b; a = b; b = c;
+    }
+
+    return {result: b, method: "цикл"};
+}
+
+function fibRecursion(number) {
+    return {result: fibN(number), method: "рекурсия"};
+}
+
+function fibN(number) {
+    return number <= 1 ? number :
+        fibN(number - 1) + fibN(number - 2);
+}
+
+function fibBinet(number) {
+    let s5 = Math.sqrt(5);
+    let s5Sum = (1 + s5) / 2;
+    let s5Diff = (1 - s5) / 2;
+    let fibNumber = 1 / s5  * (s5Sum**number - s5Diff**number);
+
+    return {result: fibNumber.toFixed(0), method: "формула Бине"};
+}
 
 function askNumber() {
     let number = "";
@@ -48,10 +78,10 @@ function execTimeTemplate(sumFunc) {
 
     if (number !== null) {
         execStart = Date.now();
-        let {sum, method} = sumFunc(+number);
+        let {result, method} = sumFunc(+number);
         execEnd = Date.now();
 
-        alert(`Результат от 0 до ${number} = ${sum}`);
+        alert(`Результат для ${number} = ${result}`);
 
         console.log(`Время выполнения (${method}):`);
         console.log(`${execEnd - execStart} мс`);
--- Vanilla/Pages/1_Language/Functions_Advanced/themes/RecursionNStack.php
+++ Vanilla/Pages/1_Language/Functions_Advanced/themes/RecursionNStack.php
@@ -20,4 +20,23 @@
             <button onclick="execTimeTemplate(sumToNProgression);">Прогрессия</button>
         </div>
     </div>
+
+    <div class="task">
+        <div class="header">Задача 1</div>
+        Написать функцию fib(n) которая возвращает n-е число Фибоначчи.
+        Сделать три варианта решения:
+        <ul>
+            <li>С использованием цикла</li>
+            <li>Через рекурсию</li>
+            <li>С использованием формулы Бине</li>
+        </ul>
+
+        <div class="header">Решение</div>
+        <div class="decision-description">Посчитать сумму (время выполнения см. в консоли):</div>
+        <div class="buttons-bar">
+            <button onclick="execTimeTemplate(fibCycle);">Цикл</button>
+            <button onclick="execTimeTemplate(fibRecursion);">Рекурсия</button>
+            <button onclick="execTimeTemplate(fibBinet);">Формула Бине</button>
+        </div>
+    </div>
 </div>
