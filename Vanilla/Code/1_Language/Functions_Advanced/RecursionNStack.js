7eb52376c99e2105f647627462d5c7726ac3b683
pew-rivza <dz-10b@ya.ru>
2020-05-13 23:20:06 +0300

    JS sum function created

--- Vanilla/Code/1_Language/Functions_Advanced/RecursionNStack.js
+++ Vanilla/Code/1_Language/Functions_Advanced/RecursionNStack.js
@@ -1,1 +1,59 @@
 'use strict';
+
+function sumToNCycle(number) {
+    let sum = 0;
+
+    for (let i = 1; i <= number; i++) {
+        sum += i;
+    }
+
+    return {sum, method: "цикл"};
+}
+
+function sumToNRecursion(number) {
+    let sum = sumTo(number);
+
+    return {sum, method: "рекурсия"};
+}
+
+function sumTo(n) {
+    return n === 0 ? n : ( n + sumTo(n - 1) );
+}
+
+function sumToNProgression(number) {
+    let sum = number * ((1 + number) / 2);
+
+    return {sum, method: "арифметическая прогрессия"};
+}
+
+
+
+function askNumber() {
+    let number = "";
+    do {
+        number = prompt("Введите целое положительное число");
+    } while
+        (!( isInt(number) && number >= 0 || number === null ));
+
+    return number;
+}
+
+function isInt(n) {
+    return (n === "0" || +n) && !( +n - Math.trunc(+n) );
+}
+
+function execTimeTemplate(sumFunc) {
+    let number = askNumber();
+    let execStart, execEnd;
+
+    if (number !== null) {
+        execStart = Date.now();
+        let {sum, method} = sumFunc(+number);
+        execEnd = Date.now();
+
+        alert(`Результат от 0 до ${number} = ${sum}`);
+
+        console.log(`Время выполнения (${method}):`);
+        console.log(`${execEnd - execStart} мс`);
+    }
+}
--- Vanilla/Pages/1_Language/Functions_Advanced/themes/RecursionNStack.php
+++ Vanilla/Pages/1_Language/Functions_Advanced/themes/RecursionNStack.php
@@ -4,8 +4,20 @@
     <h3 id="recursion-n-stack">Рекурсия и стек</h3>
     <div class="task">
         <div class="header">Задача 1</div>
+        Написать функцию sumTo(n), которая вычисляет сумму чисел 1 + 2 + ... + n.
+        Сделать три варианта решения:
+        <ul>
+            <li>С использованием цикла</li>
+            <li>Через рекурсию</li>
+            <li>С использованием формулы арифметической прогрессии</li>
+        </ul>
 
         <div class="header">Решение</div>
-        <button onclick="">DATA</button>
+        <div class="decision-description">Посчитать сумму (время выполнения см. в консоли):</div>
+        <div class="buttons-bar">
+            <button onclick="execTimeTemplate(sumToNCycle);">Цикл</button>
+            <button onclick="execTimeTemplate(sumToNRecursion);">Рекурсия</button>
+            <button onclick="execTimeTemplate(sumToNProgression);">Прогрессия</button>
+        </div>
     </div>
 </div>
