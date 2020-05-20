922f9bac92b0ea87a9d4c3bc1e659949f22041b5
pew-rivza <dz-10b@ya.ru>
2020-05-20 23:15:31 +0300

    JS Inheritance theme created

---
+++ Vanilla/Pages/1_Language/Classes/themes/Inheritance.php
@@ -0,0 +1,11 @@
+<script src="/Vanilla/Code/1_Language/Classes/Inheritance.js"></script>
+
+<div class="theme">
+    <h3 id="inheritance">Наследование классов</h3>
+    <div class="task">
+        <div class="header">Задача 1</div>
+
+        <div class="header">Решение</div>
+        <button onclick="">DATA</button>
+    </div>
+</div>
