1f74c519dcaaf32fee0aa95d928d7cdafdf78b7c
pew-rivza <dz-10b@ya.ru>
2020-05-20 23:15:11 +0300

    JS Classes page created

---
+++ Vanilla/Pages/1_Language/Classes/index.php
@@ -0,0 +1,23 @@
+<?php
+
+include_once __DIR__ . "/../../common/Head.php";
+include_once __DIR__ . "/../../common/Menu.php";
+?>
+
+<div class="chapter-container">
+    <div class="sub-menu">
+        <h3>Навигация</h3>
+        <ul>
+            <li><a href="#inheritance">Наследование классов</a></li>
+            <li><a href="#">Статические свойства и методы</a></li>
+            <li><a href="#">Приватные и защищённые методы и свойства</a></li>
+            <li><a href="#">Расширение встроенных классов</a></li>
+            <li><a href="#">Проверка класса: "instanceof"</a></li>
+            <li><a href="#">Примеси</a></li>
+        </ul>
+    </div>
+    <div class="content">
+        <h2>Классы</h2>
+        <? include_once __DIR__ . "/themes/Inheritance.php"; ?>
+    </div>
+</div>
