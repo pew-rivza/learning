54383349541bb9078a94de59bdd2282fe9322206
pew-rivza <dz-10b@ya.ru>
2020-06-17 21:52:51 +0300

    JS Browser section created (+ DocumentChanging theme)

---
+++ Vanilla/Pages/2_Browser/Document/index.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once __DIR__ . "/../../common/Head.php";
+include_once __DIR__ . "/../../common/Menu.php";
+?>
+
+<div class="chapter-container">
+    <div class="sub-menu">
+        <h3>Навигация</h3>
+        <ul>
+            <li><a href="#document-changing">Изменение документа</a></li>
+        </ul>
+    </div>
+    <div class="content">
+        <h2>Документ</h2>
+        <? include_once __DIR__ . "/themes/DocumentChanging.php"; ?>
+    </div>
+</div>
---
+++ Vanilla/Pages/2_Browser/Document/themes/DocumentChanging.php
@@ -0,0 +1,11 @@
+<script src="/Vanilla/Code/2_Browser/Document/DocumentChanging.js"></script>
+
+<div class="theme">
+    <h3 id="document-changing">Изменение документа</h3>
+    <div class="task">
+        <div class="header">Задача 1</div>
+
+        <div class="header">Решение</div>
+        <button onclick="">DATA</button>
+    </div>
+</div>
