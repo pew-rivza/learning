963a2c7072b8342b9eda369f8f2da7a696b518a4
pew-rivza <dz-10b@ya.ru>
2020-06-18 20:07:44 +0300

    JS fuckin' calendar done

--- Vanilla/Code/2_Browser/Document/DocumentChanging.js
+++ Vanilla/Code/2_Browser/Document/DocumentChanging.js
@@ -1,1 +1,94 @@
 'use strict';
+
+let data = {
+    ["Рыбы"]: {
+        ["форель"]: {},
+        ["лосось"]: {}
+    },
+
+    ["Деревья"]: {
+        ["Огромные"]: {
+            ["секвойя"]: {},
+            ["дуб"]: {}
+        },
+        ["Цветковые"]: {
+            ["яблоня"]: {},
+            ["магнолия"]: {}
+        }
+    }
+};
+
+function createTree(container, data) {
+    if (!Object.keys(data).length) return;
+
+    let ul = document.createElement('ul');
+
+    for (let key in data) {
+        let li = document.createElement('li');
+        li.textContent = key;
+        createTree(li, data[key]);
+        ul.append(li);
+    }
+
+    container.append(ul);
+
+    document.getElementById('tree').previousElementSibling.disabled = true;
+}
+
+function calcChilds() {
+    let lis = document.getElementById('animals')
+        .querySelectorAll('li');
+
+    for(let li of lis) {
+        let childsCount = li.querySelectorAll('ul li').length;
+
+        if (!childsCount) continue;
+
+        li.firstChild.data += ` [${childsCount}]`;
+    }
+
+    document.getElementById('animals').previousElementSibling.disabled = true;
+}
+
+function createCalendar(container, year, month) {
+    let date = new Date(year, month - 1, 1);
+    let table = document.createElement('table');
+    let week;
+
+    table.innerHTML = createCalendarHeader(date);
+
+    while (date.getMonth() !== month) {
+        let currentDay = date.getDay() ? date.getDay() - 1 : 6;
+
+        if (date.getDate() === 1 || !currentDay) {
+            week = createEmptyWeek();
+            table.append(week);
+        }
+
+        week.children[currentDay].textContent = date.getDate().toString();
+        date.setDate(date.getDate() + 1);
+    }
+
+    container.append(table);
+}
+
+function createEmptyWeek() {
+    let week = document.createElement('tr');
+
+    for(let i = 0; i < 7; i++) {
+        let day = document.createElement('td');
+        week.append(day);
+    }
+
+    return week;
+}
+
+function createCalendarHeader(date) {
+    let monthFormatter = new Intl.DateTimeFormat('ru', { month: 'long' });
+
+    return (
+        `<tr><th colspan="7">${ monthFormatter.format(date) }</th></tr>` +
+        '<tr><th>ПН</th><th>ВТ</th><th>СР</th><th>ЧТ</th><th>ПТ</th><th>СБ</th><th>ВС</th></tr>'
+
+    );
+}
--- Vanilla/Pages/2_Browser/Document/themes/DocumentChanging.php
+++ Vanilla/Pages/2_Browser/Document/themes/DocumentChanging.php
@@ -4,8 +4,93 @@
     <h3 id="document-changing">Изменение документа</h3>
     <div class="task">
         <div class="header">Задача 1</div>
+        Написать функцию createTree, которая создает вложенный список
+        ul/li из объекта data и вставляет его в элемент container. Преобразовать
+        в список следующий объект:
+<pre>
+let data = {
+    "Рыбы": {
+    "форель": {},
+    "лосось": {}
+    },
+
+    "Деревья": {
+        "Огромные": {
+            "секвойя": {},
+            "дуб": {}
+        },
+        "Цветковые": {
+            "яблоня": {},
+            "магнолия": {}
+        }
+    }
+};
+</pre>
 
         <div class="header">Решение</div>
-        <button onclick="">DATA</button>
+        <button onclick="createTree(document.getElementById('tree'), data);">Преобразовать</button>
+        <div id="tree" class="decision-container"></div>
+    </div>
+
+    <div class="task">
+        <div class="header">Задача 2</div>
+        Есть дерево, организованное в виде вложенных списков ul/li. Написать код,
+        который добавит каждому элементу списка количество вложенных в него элементов.
+        Узлы нижнего уровня, без детей пропускаются.
+
+        <div class="header">Решение</div>
+        <button onclick="calcChilds();">Вычислить</button>
+        <div id="animals" class="decision-container">
+            <ul>
+                <li>Животные
+                    <ul>
+                        <li>Млекопитающие
+                            <ul>
+                                <li>Коровы</li>
+                                <li>Ослы</li>
+                                <li>Собаки</li>
+                                <li>Тигры</li>
+                            </ul>
+                        </li>
+                        <li>Другие
+                            <ul>
+                                <li>Змеи</li>
+                                <li>Птицы</li>
+                                <li>Ящерицы</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li>Рыбы
+                    <ul>
+                        <li>Аквариумные
+                            <ul>
+                                <li>Гуппи</li>
+                                <li>Скалярии</li>
+                            </ul>
+                        </li>
+                        <li>Морские
+                            <ul>
+                                <li>Морская форель</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="task">
+        <div class="header">Задача 3</div>
+        Написать функцию createCalendar(elem, year, month). Вызов функции должен создать
+        календарь для заданного месяца month в году year и вставить его в elem. Календарь
+        должен быть таблицей, где неделя – это tr, а день – это td. У таблицы должен быть
+        заголовок с названием месяца, а также заголовок с названиями дней недели, каждый
+        день – th, первым днем недели должен быть понедельник.
+
+        <div class="header">Решение</div>
+        <button onclick="createCalendar(document.getElementById('calendar'), 2020, 6);">Создать календарь</button>
+        <div id="calendar" class="decision-container">
+        </div>
     </div>
 </div>
