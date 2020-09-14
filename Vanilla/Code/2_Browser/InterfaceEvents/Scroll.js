64206b9e08c9b424bd3ba1e7677007198272f3a9
pew-rivza <dz-10b@ya.ru>
2020-09-14 22:17:17 +0300

    JS Scroll theme done

--- Vanilla/Code/2_Browser/InterfaceEvents/Scroll.js
+++ Vanilla/Code/2_Browser/InterfaceEvents/Scroll.js
@@ -9,7 +9,10 @@ document.addEventListener("DOMContentLoaded", function () {
             let offsetBottom = image.offsetTop + image.offsetHeight - this.offsetHeight;
             let scrollPosition = this.scrollTop;
 
-             if (offsetTop >= scrollPosition && offsetBottom <= scrollPosition && image.dataset.src) {
+             if (offsetTop >= scrollPosition
+                 && offsetBottom <= scrollPosition
+                 && image.dataset.src) {
+
                  image.src = image.dataset.src;
                  delete image.dataset.src
              }
