3b60b618d7f9ebc7812d2aa1caaa5be6ee029d41
pew-rivza <dz-10b@ya.ru>
2021-01-08 19:02:08 +0300

    JS menu updated

--- Vanilla/Pages/common/Menu.php
+++ Vanilla/Pages/common/Menu.php
@@ -9,11 +9,31 @@
 <div class="menu-container">
     <div>
         <div class="logo">
-            <img src="/Vanilla/assets/img/logo.png">
+            <img src="/Vanilla/assets/img/logo.png" alt="">
         </div>
         <div class="menu-items">
-            <div>JavaScript</div>
-            <div>Браузер</div>
+            <div class="item">
+                JavaScript
+                <ul class="sub-items">
+                    <li><a href="/Vanilla/Pages/1_Language/JS_Basics/">Основы JavaScript</a></li>
+                    <li><a href="/Vanilla/Pages/1_Language/Objects_Basics/">Объекты: основы</a></li>
+                    <li><a href="/Vanilla/Pages/1_Language/Data_Types/">Типы данных</a></li>
+                    <li><a href="/Vanilla/Pages/1_Language/Functions_Advanced/">Продвинутая работа с функциями</a></li>
+                    <li><a href="/Vanilla/Pages/1_Language/Classes/">Классы</a></li>
+                    <li><a href="/Vanilla/Pages/1_Language/Promises/">Промисы, async/await</a></li>
+                    <li><a href="/Vanilla/Pages/1_Language/Generators/">Генераторы, продвинутая итерация</a></li>
+                    <li><a href="/Vanilla/Pages/1_Language/Other/">Разное</a></li>
+                </ul>
+            </div>
+            <div class="item">
+                Браузер
+                <ul class="sub-items">
+                    <li><a href="/Vanilla/Pages/2_Browser/Document/">Документ</a></li>
+                    <li><a href="/Vanilla/Pages/2_Browser/EventsIntro/">Введение в события</a></li>
+                    <li><a href="/Vanilla/Pages/2_Browser/InterfaceEvents/">Интерфейсные события</a></li>
+                    <li><a href="/Vanilla/Pages/2_Browser/Forms/">Формы, элементы управления</a></li>
+                </ul>
+            </div>
             <div>Advanced</div>
         </div>
     </div>
